b3c624b471c1ccda2def636bde45df6b3c69e0d2
ramhillel <ramhillel12@gmail.com>
2018-01-07 19:38:11 +0200

    personal acticity

---
+++ app/src/main/java/com/example/inbar/heimdall/PersonalStatisticsActivity.java
@@ -0,0 +1,48 @@
+package com.example.inbar.heimdall;
+
+import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.PopupWindow;
+
+import com.github.mikephil.charting.charts.PieChart;
+
+/**
+ * Created by ramhillel on 07/01/18.
+ */
+
+public class PersonalStatisticsActivity extends APIRequest {
+
+    private CoordinatorLayout mainLayout;
+    private PopupWindow mPopupWindow;
+    PieChart mChart;
+    View customView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mainLayout =  findViewById(R.id.mainLayout);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        
+    }
+}
