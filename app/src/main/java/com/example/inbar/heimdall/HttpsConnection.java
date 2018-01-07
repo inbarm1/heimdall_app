cf592885571e847c55710ee93c68321dec1767e0
oferh <ofer.handel@gmail.com>
2018-01-07 20:50:25 +0200

    fixed connection

--- app/src/main/java/com/example/inbar/heimdall/HttpsConnection.java
+++ app/src/main/java/com/example/inbar/heimdall/HttpsConnection.java
@@ -13,6 +13,7 @@ import android.widget.PopupWindow;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -22,13 +23,13 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.Iterator;
 
 
 public class HttpsConnection extends NevActivity {
 
     private final String USER_TOKEN="user_token";
     StrictMode.ThreadPolicy policy;
-    HttpURLConnection conn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,13 +41,13 @@ public class HttpsConnection extends NevActivity {
     protected HttpURLConnection getConnection(final int id_layer, String subDomain){
         try {
             URL url = new URL("http://api.heimdall.ga"+subDomain);
-            conn = (HttpURLConnection) url.openConnection();
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setReadTimeout(10000);
             conn.setConnectTimeout(15000);
             conn.setRequestMethod("POST");
             conn.setDoInput(true);
             conn.setDoOutput(true);
-            conn.setRequestProperty("Content-Type","application/json");
+            conn.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
             conn.connect();
             return conn;
         } catch (Exception e) {
@@ -78,8 +79,29 @@ public class HttpsConnection extends NevActivity {
     }
 
     private void sendToConnection(JSONObject message, HttpURLConnection connection) throws IOException {
+        StringBuffer postData = new StringBuffer();
+        try {
+            for (Iterator<String> it = message.keys(); it.hasNext(); ) {
+                if (it.hasNext()) {
+                    String name = it.next();
+                    postData.append(URLEncoder.encode(name, "UTF-8"));
+                    postData.append("=");
+                    Object value;
+                    if(message.get(name) instanceof String)
+                        value = URLEncoder.encode(message.getString(name), "UTF-8");
+                    else
+                        value = message.get(name);
+                    postData.append(value);
+                    if(it.hasNext())
+                        postData.append("&");
+                }
+
+            }
+        }catch (JSONException e){
+            throw new RuntimeException(e);
+        }
         DataOutputStream out = new DataOutputStream(connection.getOutputStream ());
-        out.writeBytes(URLEncoder.encode(message.toString(),"UTF-8"));
+        out.writeBytes(postData.toString());
         out.flush ();
         out.close ();
     }
