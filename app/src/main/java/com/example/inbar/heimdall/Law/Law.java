086e2bfba8b082f019c7fe2dfb425852f7916f0c
ramhillel <ramhillel12@gmail.com>
2018-01-12 13:34:49 +0200

    law get user distibution

--- app/src/main/java/com/example/inbar/heimdall/Law/Law.java
+++ app/src/main/java/com/example/inbar/heimdall/Law/Law.java
@@ -2,7 +2,16 @@ package com.example.inbar.heimdall.Law;
 
 import android.graphics.drawable.Drawable;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by Eilon on 05/01/2018.
@@ -13,18 +22,71 @@ public class Law {
     public static final String DESC = "description";
     public static final String TAGS = "tags";
     public static final String USER_VOTED = "user_voted";
+    public static final String JOB_FOR = "job_for";
+    public static final String JOB_AGAINST = "job_against";
+    public static final String RESIDENT_FOR = "resident_for";
+    public static final String RESIDENT_AGAINST = "resident_against";
+    public static final String AGE_FOR = "age_for";
+    public static final String AGE_AGAINST = "age_against";
 
 
 
-    public String name;
-    public VoteStatus voteStat;
-    public String description;
-    public String link;
+    private String name;
+    private VoteStatus voteStat;
+    private String description;
+    private String link;
+    private ArrayList<String> tags;
+    private HashMap<String, HashMap<String, Float>> userDist;
 
-    public Law(){}
+    public Law(String name, JSONObject lawObject) {
+        this.name = name;
+        try {
+            this.voteStat = VoteStatus.valueOf(lawObject.getString(USER_VOTED).toUpperCase());
+            this.description = lawObject.getString(DESC);
+            this.link = lawObject.getString(LINK);
+            this.tags = getTagsAsArray(lawObject.getJSONArray(TAGS));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 
-    public Law(JSONObject lawObject) {
+    public String getName() {
+        return name;
+    }
 
+    public VoteStatus getVoteStat() {
+        return voteStat;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public ArrayList<String> getTags() {
+        return tags;
+    }
+
+    private ArrayList<String> getTagsAsArray(JSONArray jArray) throws JSONException {
+        ArrayList<String> tags = new ArrayList<>();
+        if (jArray == null) return tags;
+        for (int i = 0; i < jArray.length(); i++) {
+            tags.add(jArray.getString(i));
+        }
+        return tags;
+    }
+
+
+    public void setUserDist(JSONObject distObject) {
+        Type type = new TypeToken<HashMap<String, HashMap<String, Float>>>(){}.getType();
+        userDist = new Gson().fromJson(distObject.toString(), type);
+    }
+
+    public HashMap<String, HashMap<String, Float>> getUserDist() {
+        return userDist;
     }
 
 }
--- app/src/main/java/com/example/inbar/heimdall/Law/VoteStatus.java
+++ app/src/main/java/com/example/inbar/heimdall/Law/VoteStatus.java
@@ -1,13 +1,7 @@
 package com.example.inbar.heimdall.Law;
 
 public enum VoteStatus {
-    FOR("for"),
-    AGAINST("against"),
-    NO_VOTE("no_vote");
-
-    private final String str;
-
-    VoteStatus(String str) {
-        this.str = str;
-    }
+    FOR,
+    AGAINST,
+    NO_VOTE
 }
