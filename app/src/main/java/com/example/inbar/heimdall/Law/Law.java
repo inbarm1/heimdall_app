7266dc43710c8dfade3b99d40ad170f12f1dccb8
ramhillel <ramhillel12@gmail.com>
2018-01-12 12:21:17 +0200

    law constructor

--- app/src/main/java/com/example/inbar/heimdall/Law/Law.java
+++ app/src/main/java/com/example/inbar/heimdall/Law/Law.java
@@ -2,23 +2,29 @@ package com.example.inbar.heimdall.Law;
 
 import android.graphics.drawable.Drawable;
 
+import org.json.JSONObject;
+
 /**
  * Created by Eilon on 05/01/2018.
  */
 
 public class Law {
+    public static final String LINK = "link";
+    public static final String DESC = "description";
+    public static final String TAGS = "tags";
+    public static final String USER_VOTED = "user_voted";
+
+
+
     public String name;
     public VoteStatus voteStat;
     public String description;
-    public Drawable image;
+    public String link;
 
     public Law(){}
 
-    public Law(String name, VoteStatus status, String description, Drawable image) {
-        this.name = name;
-        this.voteStat = status;
-        this.description = description;
-        this.image = image;
+    public Law(JSONObject lawObject) {
+
     }
 
 }
--- app/src/main/java/com/example/inbar/heimdall/Law/VoteStatus.java
+++ app/src/main/java/com/example/inbar/heimdall/Law/VoteStatus.java
@@ -1,7 +1,13 @@
 package com.example.inbar.heimdall.Law;
 
 public enum VoteStatus {
-    FOR,
-    AGAINST,
-    ABSTAIN
+    FOR("for"),
+    AGAINST("against"),
+    NO_VOTE("no_vote");
+
+    private final String str;
+
+    VoteStatus(String str) {
+        this.str = str;
+    }
 }
