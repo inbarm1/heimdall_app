856937346b02281bd62c8540bcbe933f88fe57e9
eilonweinfeld <eilonweinfeld.ew@gmail.com>
2018-01-13 11:15:12 +0200

    "merge in the morning"

--- app/src/main/java/com/example/inbar/heimdall/Law/LawActivity.java
+++ app/src/main/java/com/example/inbar/heimdall/Law/LawActivity.java
@@ -1,8 +1,11 @@
 package com.example.inbar.heimdall.Law;
 
 import android.app.DialogFragment;
+import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -13,18 +16,33 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.app.FragmentManager;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.PopupWindow;
 import android.widget.Toast;
 
 import com.example.inbar.heimdall.APIRequest;
 import com.example.inbar.heimdall.HttpsConnection;
 import com.example.inbar.heimdall.R;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+
+import static android.graphics.Color.rgb;
 
 public class LawActivity extends APIRequest {
     private RecyclerView mRecyclerView;
@@ -39,7 +57,7 @@ public class LawActivity extends APIRequest {
         setSupportActionBar(toolbar);
 
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
-        mLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+        mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setNestedScrollingEnabled(false);
         mAdapter = new LawListAdapter(getLaws(), this);
@@ -57,12 +75,12 @@ public class LawActivity extends APIRequest {
 
     public void showDatePickerDialog(View v) {
         DialogFragment newFragment = new Utils.DatePickerFragment();
-        newFragment.show(getFragmentManager(),"datePicker");
+        newFragment.show(getFragmentManager(), "datePicker");
     }
 
-    public Law[] getLaws(){
+    public Law[] getLaws() {
         Law[] laws = new Law[30];//=  ;//Util.getPeopleList(this);
-        for (int i =0; i < 30 ; i++){
+        for (int i = 0; i < 30; i++) {
             Law l1 = null;
             try {
                 l1 = createLaw(i);
@@ -75,18 +93,17 @@ public class LawActivity extends APIRequest {
     }
 
     public static Law createLaw(int i) throws JSONException {
-        String jsonS = String.format("{'fuck law %s': {'link' : 'www.pornhub.com, 'description' : 'bla bla', 'tags' : ['eilon','ram'], 'user_voted' : 'for'  }",i);
+        String jsonS = String.format("{'fuck law %s': {'link' : 'www.pornhub.com, 'description' : 'bla bla', 'tags' : ['eilon','ram'], 'user_voted' : 'for'  }", i);
         JSONObject subJson = new JSONObject();
-        subJson.put("link","pornhub.com " + String.valueOf(i));
-        subJson.put("description","bla bla "+ String.valueOf(i));
+        subJson.put("link", "pornhub.com " + String.valueOf(i));
+        subJson.put("description", "bla bla " + String.valueOf(i));
         List<String> tags = new ArrayList<>();
-        tags.add("eilon "+ String.valueOf(i));
-        tags.add("the king "+ String.valueOf(i));
-        subJson.put("tags",tags);
-        subJson.put("user_voted","for");
-        return new Law("law " + String.valueOf(i),subJson );
+        tags.add("eilon " + String.valueOf(i));
+        tags.add("the king " + String.valueOf(i));
+        subJson.put("tags", tags);
+        subJson.put("user_voted", "for");
+        return new Law("law " + String.valueOf(i), subJson);
     }
-
 }
 
 
